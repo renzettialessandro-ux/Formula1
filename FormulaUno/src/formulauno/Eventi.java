1727c97a066a3aeeaebdba76e66eb16ef6a9125f
renzettialessandro-ux <renzetti.alessandro@franchettisalviani.net>
2026-02-16 12:47:57 +0100

    gfhfgh

---
+++ FormulaUno/src/formulauno/Eventi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package formulauno;
+
+/**
+ *
+ * @author renzetti.alessandro
+ */
+public enum Eventi {
+    RAIN,
+    DNF,
+    ALA_ROTTA,
+    SAFETY_CAR
+}
---
+++ FormulaUno/src/formulauno/Gara.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package formulauno;
+
+/**
+ *
+ * @author renzetti.alessandro
+ */
+public class Gara {
+    
+}
